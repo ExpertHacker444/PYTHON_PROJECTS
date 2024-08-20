ba06669cc2075bc89881d3e9db9d62eb57111084
Axel Cardozo <kakashigame4564@gmail.com>
2024-08-19 22:26:42 -0300

    metodos y ejercicios

---
+++ POO con DALTO/ejercicio1-2.py
@@ -0,0 +1,27 @@
+class Estudiante:
+    def __init__(self, nombre, edad, ano):
+        self.nombre = nombre
+        self.edad = edad
+        self.ano = ano
+
+    def estudiar(self):
+        print("Estudiante en curso...")
+
+  
+nombre = input("ingrese su nombre:")
+edad = input("ingrese su edad: ")
+ano = input("ingrese su año: ")
+
+estudiantes = Estudiante(nombre, edad, ano)
+
+print(f"""
+      Datos del Estudiante: \n
+      Nombre: {estudiantes.nombre}
+      Edad: {estudiantes.edad}
+      Año: {estudiantes.ano}
+""")
+
+while True:
+    estudiar1 = input("esta estudiando? Ingrese la palabras estudiar: ")
+    if estudiar1 == "estudiar":
+        estudiantes.estudiar()
